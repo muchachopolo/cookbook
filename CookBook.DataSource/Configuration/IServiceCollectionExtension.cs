d0afd4bc20c975b8a7f8679b692e1b3c6739494d
Pablo Daza <aketza.daza@me.com>
2019-07-20 01:19:36 -0400

    - new repository to manage the data - in memory database configuration - IServiceCollection implementation to register the new repository implementation into the dependency container - project dependencies added

---
+++ CookBook.DataSource/Configuration/IServiceCollectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CookBook.DataSource.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CookBook.DataSource.Configuration
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection AddDataSourceRepositories(this IServiceCollection services)
+        {
+            services.AddDbContext<DataRepository>(options =>
+            {
+                options.UseInMemoryDatabase("CookBook");
+            });
+            return services;
+        }
+    }
+}
---
+++ CookBook.DataSource/Repository/DataRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CookBook.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookBook.DataSource.Repository
+{
+    public class DataRepository : DbContext
+    {
+        public DataRepository(DbContextOptions<DataRepository> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
