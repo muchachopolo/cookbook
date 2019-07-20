c5a7d374e02669472fd63a664dbefeb868b41599
Pablo Daza <aketza.daza@me.com>
2019-07-20 01:20:06 -0400

    - new user model added

---
+++ CookBook.Entities/Users/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CookBook.Entities.Users
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birth { get; set; }
+    }
+}
