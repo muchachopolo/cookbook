46a4bec6e0e6be2b7c875cbb52089831d07ab9c9
Pablo Daza <aketza.daza@me.com>
2019-07-20 01:21:38 -0400

    - new page login scaffold - basic model biding to manage login page

--- CookBook.Web/Pages/Index.cshtml.cs
+++ CookBook.Web/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CookBook.Entities.Users;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -10,6 +11,14 @@ namespace CookBook.Web.Pages
     public class IndexModel : PageModel
     {
         public void OnGet()
+        {
+
+        }
+
+        [BindProperty]
+        public User User { get; set; }
+
+        public void OnPost()
         {
 
         }
