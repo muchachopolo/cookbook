75509e729c1d959b3ce805fd2954e53c565dd7ba
Pablo Daza <aketza.daza@me.com>
2019-07-20 22:20:36 -0400

    Profile functionality

--- CookBook.Services/Users/UserService.cs
+++ CookBook.Services/Users/UserService.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using CookBook.DataSource.Repository;
 using CookBook.Entities.Users;
+using Microsoft.EntityFrameworkCore;
 
 namespace CookBook.Services.Users
 {
@@ -19,6 +21,20 @@ namespace CookBook.Services.Users
             await _repository.Users.AddAsync(user);
             await _repository.SaveChangesAsync();
             return user;
+        }
+
+        public async Task<User> GetUserAsync(int? id)
+        {
+            var User = await _repository.Users.Select(user => new User
+            {
+                Id = user.Id,
+                Email = user.Email,
+                active = user.active,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Birth = user.Birth
+            }).FirstOrDefaultAsync(user => user.Id == id);
+            return User;
         }
     }
 }
---
+++ CookBook.ViewModels/Users/Profile.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookBook.Entities.Users;
+using CookBook.Services.Users;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CookBook.ViewModels.Users
+{
+    public class ProfileModel : PageModel
+    {
+        private readonly UserService userService;
+        public User User { get; set; }
+
+        public ProfileModel(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User = await userService.GetUserAsync(id);
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
